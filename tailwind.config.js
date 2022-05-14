6a633fc0972f3636aa62f59cfc667f7763ee2cff
Sauli Kylmänen <62215427+saukkko@users.noreply.github.com>
2022-05-14 12:43:27 +0300

    back to tailwindcss with purecss

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    fontFamily: {
+      roboto: ["Roboto", "system-ui", "sans-serif"],
+      "open-sans": ['"Open Sans"', "system-ui", "sans-serif"],
+      "roboto-mono": ['"Roboto Mono"', "monospace"],
+    },
+    colors: {
+      moccasin: "#ffe4b5",
+      whitesmoke: "#f5f5f5",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
